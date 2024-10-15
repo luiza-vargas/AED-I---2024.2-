113a8a2ade4d2cb24c04e3bc3f8b807156795641
luiza-vargas <luiza.vargas@unifesp.br>
2024-10-15 14:45:38 -0300

    Update Exemplo aula 15.10 - ponteiros.c

--- Exemplo aula 15.10 - ponteiros.c
+++ Exemplo aula 15.10 - ponteiros.c
@@ -8,11 +8,11 @@ int main()
 {
   int x=1,y=3;
 
-	/*declara dois ponteiros que armazenam o endereC'o de uma variC!vel tipo int e inicializa
-	eles com o endereC'o de y, assim px e py */
+	/*declara dois ponteiros que armazenam o endereco de uma variavel tipo int e inicializa
+	eles com o endereco de y, assim px e py */
 	int*px=&y,*py=&y;
 
-	/*soma os valores apontados pelos ponteiros px e py e armazena o resultado no endereC'o apontado por px.
+	/*soma os valores apontados pelos ponteiros px e py e armazena o resultado no endereco apontado por px.
 	O valor de y ser dobrado*/
 	*px=*px+*py;
 
@@ -22,10 +22,10 @@ int main()
 	/*imprime os valores apontados pelos ponteiros px e py*/
 	printf("*px=%d, *py=%d\n", *px, *py);
 
-	/*imprime os endereC'os armazenados em px e py*/
+	/*imprime os enderecos armazenados em px e py*/
 	printf("px=%p, py=%p\n", px, py);
 
-	/*imprime o endereC'o das variaveis x e y */
+	/*imprime o endereco das variaveis x e y */
 	printf("&x=%p, &y=%p\n", &x, &y);
 
 	return 0;

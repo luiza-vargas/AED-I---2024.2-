58eaa0023e981ddb94c83f808d9e1e0606b397c4
luiza-vargas <luiza.vargas@unifesp.br>
2024-10-28 11:10:25 -0300

    1383.c
    sudoku

---
+++ 1383.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+int verifica_linha_99(int matriz[9][9]) {
+    int i, j, k;
+
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            if (matriz[i][j] > 9 || matriz[i][j] < 1) {  // verifica se algum número maior que 9 ou menor que 1
+                return 0;
+            }
+
+            for (k = j + 1; k < 9; k++) {
+                if (matriz[i][j] == matriz[i][k]) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int verifica_coluna_99(int matriz[9][9]) {
+    int i, j, k;
+
+    for (j = 0; j < 9; j++) {
+        for (i = 0; i < 9; i++) {
+            for (k = 0; k < 9; k++) {
+                if (i != k && matriz[i][j] == matriz[k][j]) {
+                    return 0;
+                }
+                if (matriz[i][j] > 9 || matriz[k][j] > 9) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int monta_matriz_33(int matriz[9][9]) {
+    int i, j, k, l;
+    int nova[3][3];
+
+    // Criando as submatrizes 3x3
+    for (i = 0; i < 9; i += 3) {
+        for (j = 0; j < 9; j += 3) {
+            for (k = 0; k < 3; k++) {
+                for (l = 0; l < 3; l++) {
+                    nova[k][l] = matriz[k + i][l + j];
+                }
+            }
+            if (verifica_matriz_33(nova) != 1) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int verifica_matriz_33(int nova[3][3]) {
+    int num, i, j, qnt;
+
+    for (num = 1; num <= 9; num++) {
+        qnt = 0;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if (nova[i][j] == num) {
+                    qnt++;
+                }
+            }
+        }
+        if (qnt > 1) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int n, i, j, matriz[9][9], aux;
+    scanf("%d", &n);
+
+    for (aux = 1; aux <= n; aux++) {
+        for (i = 0; i < 9; i++) {
+            for (j = 0; j < 9; j++) {
+                scanf("%d", &matriz[i][j]);
+            }
+        }
+
+        printf("Instancia %d\n", aux);
+
+        if (verifica_linha_99(matriz) && verifica_coluna_99(matriz) && monta_matriz_33(matriz)) {
+            printf("SIM\n");
+        } else {
+            printf("NAO\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}

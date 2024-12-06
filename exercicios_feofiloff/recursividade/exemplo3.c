40efc453735708fb6c06935e53fd7dc47c375d17
luiza-vargas <luiza.vargas@unifesp.br>
2024-12-06 14:33:08 -0300

    fibonacci

---
+++ exercicios_feofiloff/recursividade/exemplo3.c
@@ -0,0 +1,21 @@
+        //fibonacci
+        #include<stdio.h>
+        #include<stdlib.h>
+
+        int fibonacci(int n){
+             if(n==1){
+                return 0;
+             }
+             else{
+                if(n==2){
+                    return 1;
+                }
+                return fibonacci(n-1)+ fibonacci(n-2); 
+             }
+         }
+
+        int main(){
+            int n;
+            scanf("%d",&n);
+            printf("%d",fibonacci(n));
+        }

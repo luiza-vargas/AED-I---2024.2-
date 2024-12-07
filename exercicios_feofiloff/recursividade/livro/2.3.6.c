e9d674ea69e02a01b9bef7bbf7aed93f2e9549a3
luiza-vargas <luiza.vargas@unifesp.br>
2024-12-06 23:29:59 -0300

    .
    fibonacci

---
+++ exercicios_feofiloff/recursividade/livro/2.3.6.c
@@ -0,0 +1,12 @@
+/*fibonacci*/
+        int fibonacci(int n){
+             if(n==1){
+                return 0;
+             }
+             else{
+                if(n==2){
+                    return 1;
+                }
+                return fibonacci(n-1)+ fibonacci(n-2); 
+             }
+         }

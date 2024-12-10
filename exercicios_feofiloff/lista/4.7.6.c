2c4da57f53b5249264fac9e7a5bd2503923630eb
luiza-vargas <luiza.vargas@unifesp.br>
2024-12-10 11:04:24 -0300

    Update 4.7.6.c

--- exercicios_feofiloff/lista/4.7.6.c
+++ exercicios_feofiloff/lista/4.7.6.c
@@ -1,6 +1,6 @@
 /*Contagem. Escreva uma função que conte o número de células de uma lista
 encadeada.*/
-    int num_celulas(celula*p){
+    int * num_celulas(celula*p){
         int qntd=1;
         celula*aux;
         for(aux=p;aux!=NULL;aux=aux->prox){

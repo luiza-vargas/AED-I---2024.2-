b7f56ff873aae8ca1e6a6182ca1434fcedad776c
luiza-vargas <luiza.vargas@unifesp.br>
2025-01-11 23:00:05 -0300

    Create 3357.c

---
+++ beecrowd/3357.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+    int N; 
+    float L, Q; 
+    char nomes[10][13]; 
+    int rodadas, ricoIndex;
+    float sobra;
+
+    scanf("%d %f %f", &N, &L, &Q);
+    for (int i = 0; i < N; i++) {
+        scanf("%s", nomes[i]);
+    }
+
+    rodadas = (int)(L / Q); // Número total de cuias completas
+    sobra = fmod(L, Q); // Quantidade de água na última cuia
+
+    // Determina o índice do participante que recebe a última cuia
+    if (sobra > 0) {
+        ricoIndex = rodadas % N; // Última cuia vai para este índice
+    } else {
+        ricoIndex = (rodadas - 1) % N; // Última cuia é a anterior
+        sobra = Q; // Última cuia é completa
+    }
+
+    printf("%s %.1f\n", nomes[ricoIndex], sobra);
+
+    return 0;
+}

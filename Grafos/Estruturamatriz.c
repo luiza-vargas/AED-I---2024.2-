f940409214a080ac23932ec53e37b9f4aa1bd4fa
luiza-vargas <luiza.vargas@unifesp.br>
2025-01-22 09:22:50 -0300

    Update Estruturamatriz.c

--- Grafos/Estruturamatriz.c
+++ Grafos/Estruturamatriz.c
@@ -1,15 +1,20 @@
 /* Vértices de grafos são representados por objetos do tipo vertex. */
 #define vertex int
+
 /* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A estrutura graph representa um grafo. 
 O campo adj é um ponteiro para a matriz de adjacências do grafo. 
 O campo V contém o número de vértices e o campo A contém o número de arcos do grafo. */
+
 struct graph {
-   int V; 
-   int A; 
-   int **adj; 
+   int V;     // Número de vértices
+   int A;     // Número de arcos (arestas)
+   int **adj; // Matriz de adjacência
 };
+
+
 /* Um Graph é um ponteiro para um graph, ou seja, um Graph contém o endereço de um graph. */
 typedef struct graph *Graph;
+
 /* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHinit() constrói um grafo com vértices 0 1 .. V-1 e nenhum arco. */
 Graph GRAPHinit( int V) { 
    Graph G = malloc( sizeof *G);
@@ -28,7 +33,8 @@ static int **MATRIXint( int r, int c, int val) {
          m[i][j] = val;
    return m;
 }
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHinsertArc() insere um arco v-w no grafo G. A função supõe que v e w são distintos, positivos e menores que G->V. 
+
+/* A função GRAPHinsertArc() insere um arco v-w no grafo G. A função supõe que v e w são distintos, positivos e menores que G->V. 
 Se o grafo já tem um arco v-w, a função não faz nada. */
 void GRAPHinsertArc( Graph G, vertex v, vertex w) { 
    if (G->adj[v][w] == 0) {
@@ -36,7 +42,7 @@ void GRAPHinsertArc( Graph G, vertex v, vertex w) {
       G->A++;
    }
 }
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHremoveArc() remove do grafo G o arco v-w. A função supõe que v e w são distintos, positivos e menores que G->V. 
+/*  A função GRAPHremoveArc() remove do grafo G o arco v-w. A função supõe que v e w são distintos, positivos e menores que G->V. 
 Se não existe arco v-w, a função não faz nada. */
 void GRAPHremoveArc( Graph G, vertex v, vertex w) { 
    if (G->adj[v][w] == 1) {
@@ -44,7 +50,7 @@ void GRAPHremoveArc( Graph G, vertex v, vertex w) {
       G->A--;
    }
 }
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHshow() imprime, para cada vértice v do grafo G, em uma linha, todos os vértices adjacentes a v. */
+/*  A função GRAPHshow() imprime, para cada vértice v do grafo G, em uma linha, todos os vértices adjacentes a v. */
 void GRAPHshow( Graph G) { 
    for (vertex v = 0; v < G->V; ++v) {
       printf( "%2d:", v);

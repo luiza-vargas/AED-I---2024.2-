17e5b6dd24663aa7dd74e22bafe0e1b9b1edf063
luiza-vargas <luiza.vargas@unifesp.br>
2025-01-25 15:36:45 -0300

    Update 2006.c

--- beecrowd/2006.c
+++ beecrowd/2006.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
- 
-int main() {
-    
- int certo,aux,cont=0;
- int resp[5];
- 
- scanf("%d",&certo);
- 
- for (aux = 0; aux < 5; aux++) {
-        scanf("%d", &resp[aux]);
+    #include <stdio.h>
+        
+    int main() {
+            
+        int certo, aux, cont = 0;
+        int resp[5];
+        
+        scanf("%d", &certo);
+        
+        for (aux = 0; aux < 5; aux++) {
+            scanf("%d", &resp[aux]);
+        }
+            
+        for (aux = 0; aux < 5; aux++) {
+            if (certo == resp[aux]) {
+                cont++;
+            }
+        }
+        
+        printf("%d\n", cont);
+        
+        return 0;
     }
-    
- for(aux=0;aux<5;aux++){
-     if(certo==resp[aux]){
-         cont++;
-     }
- }
- 
- printf("%d\n",cont);
- 
-    return 0;
-}

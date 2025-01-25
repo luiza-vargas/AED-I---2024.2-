3b3355d98ccc425fb635998cd8811959cb944339
luiza-vargas <luiza.vargas@unifesp.br>
2025-01-25 15:32:14 -0300

    Update 1793.c

--- beecrowd/1793.c
+++ beecrowd/1793.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main() {
-    int N;
+    int main() {
+        int N;
 
-    while (1) {
+        while (1) {
 
-        scanf("%d", &N);
-        if (N == 0) break;
+            scanf("%d", &N);
+            if (N == 0) break;
 
-        int tempos[N];
+            int tempos[N];
 
-        for (int i = 0; i < N; i++) {
-            scanf("%d", &tempos[i]);
-        }
-
-        int tempo_ativo = 0;
-        int fim_ativo = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (tempos[i] > fim_ativo) {
-
-                tempo_ativo += 10;
-                fim_ativo = tempos[i] + 9;
-            } else {
-      
-                tempo_ativo += (tempos[i] + 9 - fim_ativo);
-                fim_ativo = tempos[i] + 9;
+            for (int i = 0; i < N; i++) {
+                scanf("%d", &tempos[i]);
             }
-        }
-        printf("%d\n", tempo_ativo);
-    }
 
-    return 0;
-}
+            int tempo_ativo = 0;
+            int fim_ativo = 0;
+
+            for (int i = 0; i < N; i++) {
+                if (tempos[i] > fim_ativo) {
+
+                    tempo_ativo += 10;
+                    fim_ativo = tempos[i] + 9;
+                } else {
+        
+                    tempo_ativo += (tempos[i] + 9 - fim_ativo);
+                    fim_ativo = tempos[i] + 9;
+                }
+            }
+            printf("%d\n", tempo_ativo);
+        }
+
+        return 0;
+    }

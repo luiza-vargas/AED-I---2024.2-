216f1e02d50809fa15717542b27949838f4d3939
luiza-vargas <luiza.vargas@unifesp.br>
2025-01-25 15:34:55 -0300

    Update 1930.c

--- beecrowd/1930.c
+++ beecrowd/1930.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
- 
-int main() {
-    
- int t1,t2,t3,t4,aux,max;
- 
- scanf("%d %d %d %d",&t1,&t2,&t3,&t4);
- 
- printf("%d\n",t1+t2+t3+t4-3);
+    #include <stdio.h>
 
-    return 0;
-}
+    int main() {
+        int t1, t2, t3, t4;
+
+        scanf("%d %d %d %d", &t1, &t2, &t3, &t4);
+
+        printf("%d\n", t1 + t2 + t3 + t4 - 3);
+
+        return 0;
+    }

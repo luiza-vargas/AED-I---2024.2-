795160645d163d2916c8d6fc971e201c498c34d9
luiza-vargas <luiza.vargas@unifesp.br>
2025-01-25 15:19:06 -0300

    Update 1013.c

--- beecrowd/1013.c
+++ beecrowd/1013.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
- 
-int main() {
- int a,b,c,maiorac,aux;
- scanf("%d %d %d",&a,&b,&c);
- aux=(a+b+abs(a-b))/2;
- 
-  maiorac=(aux+c+abs(aux-c))/2;
+    #include <stdio.h>
+    
+    int main() {
+    int a,b,c,maiorac,aux;
+    scanf("%d %d %d",&a,&b,&c);
+    aux=(a+b+abs(a-b))/2;
+    
+    maiorac=(aux+c+abs(aux-c))/2;
 
- printf("%d eh o maior\n",maiorac);
-    return 0;
-}
+    printf("%d eh o maior\n",maiorac);
+        return 0;
+    }
